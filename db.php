54ec4d3311ac37f50dee6fd08dc3730542d37a22
Uma Sri <garagaumasri@gmail.com>
2026-05-22 21:39:59 +0530

    Initial commit - Student Registration System

---
+++ db.php
@@ -0,0 +1,7 @@
+<?php
+$conn = new mysqli("localhost", "root", "", "student_db", 3307);
+
+if ($conn->connect_error) {
+    die("Connection failed");
+}
+?>
---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Student Registration Form</title>
+</head>
+<body>
+
+<h2>Student Registration Form</h2>
+
+<form method="POST" action="insert.php">
+
+    Name:<br>
+    <input type="text" name="name"><br><br>
+
+    Email:<br>
+    <input type="email" name="email"><br><br>
+
+    Course:<br>
+    <input type="text" name="course"><br><br>
+
+    <input type="submit" value="Register">
+
+</form>
+
+</body>
+</html>
